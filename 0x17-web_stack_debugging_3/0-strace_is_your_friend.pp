14dd968ad778012d57db9ceb912acd864df38424
Echimau <jerryjohne77@gmail.com>
2024-09-26 18:10:53 -0700

    upload

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes a WordPress configuration issue causing Apache 500 error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+}
